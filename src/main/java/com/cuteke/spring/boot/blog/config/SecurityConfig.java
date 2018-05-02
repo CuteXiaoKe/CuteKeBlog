413e27faa7f8fadb905a977512a8442c6dc54101
CuteKe <839713259@qq.com>
2018-05-02 09:19:51 +0800

    js/css/jpg cache optimization

--- src/main/java/com/cuteke/spring/boot/blog/config/SecurityConfig.java
+++ src/main/java/com/cuteke/spring/boot/blog/config/SecurityConfig.java
@@ -59,7 +59,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	            .and().rememberMe().key(KEY) // 启用 remember me
 	            .and().exceptionHandling().accessDeniedPage("/403");  // 处理异常，拒绝访问就重定向到 403 页面
 	    http.csrf().ignoringAntMatchers("/h2-console/**"); // 禁用 H2 控制台的 CSRF 防护
-	    http.headers().frameOptions().sameOrigin(); // 允许来自同一来源的H2 控制台的请求
+	    http.headers().frameOptions().sameOrigin();// 允许来自同一来源的H2 控制台的请求
+		             // .and().headers().defaultsDisabled().cacheControl();  原生的开启cacheControl
 	}
 	
 	/**
